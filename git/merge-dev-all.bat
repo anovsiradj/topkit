fc6e5d6fee5793165df005b65e013fab4ddb8e56
anovsiradj <anov.siradj22@gmail.com>
2025-12-31 15:11:23 +0700

    tweak

--- git/merge-dev-all.bat
+++ git/merge-dev-all.bat
@@ -1,11 +1,14 @@
 git checkout dev
+git pull origin dev
 git push origin dev
 
 git checkout test
+git pull origin test
 git merge dev
 git push origin test
 
 git checkout prod
+git pull origin prod
 git merge test
 git push origin prod
 
--- git/merge-test-prod.bat
+++ git/merge-test-prod.bat
@@ -1,7 +1,9 @@
 git checkout test
+git pull origin test
 git push origin test
 
 git checkout prod
+git pull origin prod
 git merge test
 git push origin prod
 
