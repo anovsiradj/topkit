cc99ba99c25a5f737f7d58300d3e660dae23c6c5
anovsiradj <anov.siradj22@gmail.com>
2026-01-06 11:50:57 +0700

    git merge

--- git/merge-all.bat
+++ git/merge-all.bat
@@ -1,9 +1,14 @@
+git checkout dev
+git pull origin dev
+
 git checkout test
 git pull origin test
+git merge dev --no-edit
+git push origin test
 
 git checkout prod
 git pull origin prod
 git merge test --no-edit
 git push origin prod
 
-git checkout test
+git checkout dev
--- git/merge-dev-test.bat
+++ git/merge-dev-test.bat
@@ -1,15 +1,9 @@
 git checkout dev
 git pull origin dev
-git push origin dev
 
 git checkout test
 git pull origin test
-git merge dev
+git merge dev --no-edit
 git push origin test
-
-git checkout prod
-git pull origin prod
-git merge test
-git push origin prod
 
 git checkout dev
